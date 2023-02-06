552a6a88c8b0e39b21cc7efebd015dc0af625857
ashley arena <brandonarena@Brandons-MacBook-Pro.local>
2023-02-06 14:15:34 -0800

    push to main

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+// Host public folder
+app.use(express.static('public'));
+
+// Parse URL encoded & JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
